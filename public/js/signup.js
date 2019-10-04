aac9a5c6013ab02338826101fadb01fa2a6e793a
olanrewaju <olanrewajuadesanya234@gmail.com>
2019-10-05 05:59:57 +0800

    js signup validation

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,3 +1,52 @@
+// userinterface validation using javascript  
+$(document).ready(function(){
+   $("form").submit(function(e){
+    var email = $("#email").val();
+    var pswd = $("#password").val();
+    var pass = $("#password-confirm").val();
+    var name = $("#name").val();
+                        
+   if ((email =="") && (pass == "") && (pswd == "")){
+        $('#say').html("Fill in the required fields*");
+        e.preventDefault();
+          }
+     else if ((email!="" && pswd != "") && pass == ""){
+       $("#password-confirm").css("border", "1px solid red");
+        e.preventDefault();
+           }
+     else if ((email!="" && pass != "") && pswd == ""){
+       $("#password").css("border", "1px solid red");
+           e.preventDefault();
+        }else if (email!="" && pswd != pass){
+          $("#say").html("Password did not match");
+           e.preventDefault();
+          }
+     else if ((pswd!="" && pass != "") && email == ""){
+        $("#email").css("border", "1px solid red");
+       e.preventDefault();
+         }
+     else if ((pswd =="" && pass == "") && email != ""){
+        $("#password").css("border", "1px solid red");
+       $("#password-confirm").css("border", "1px solid red");
+         e.preventDefault();
+         }
+      else if ((email =="" && pass == "") && pswd != ""){
+      $("#email").css("border", "1px solid red");
+      $("#password-confirm").css("border", "1px solid red");
+         e.preventDefault();
+         }
+      else if ((email =="" && pswd == "") && pass != ""){
+       $("#email").css("border", "1px solid red");
+       $("#password").css("border", "1px solid red");
+         e.preventDefault();
+        }else{
+          return ('submit');
+             }
+          });
+      });
+         
+
+
 //password match validation through disabling submit button if passwords do not match//
 
 function checkPassword () {
